e5403db061175184169a199091e56fa505ba28f5
Zachary Scott <mail@zzak.io>
2017-09-22 19:29:28 +0900

    Refactor SHA1/Proc report_name options tests

--- test/minitest/test_options.rb
+++ test/minitest/test_options.rb
@@ -1,62 +1,61 @@
 require 'minitest/autorun'
 require 'minitest/ci'
-
 require 'stringio'
 
-class MockTestSha1ReportNameSuite < Minitest::Test
+TestOptionsDescription = 'report with this name should be transformed in sha1'
+
+class TestPass < Minitest::Test
   def test_pass
     pass
   end
 end
 
-describe 'report with this name should be transformed in sha1' do
- it 'passes' do
-   pass
- end
+describe TestOptionsDescription do
+  it 'passes' do
+    pass
+  end
 end
 
+# Setup tests reporter using sha1
 $ci_io = StringIO.new
 Minitest::Ci.clean = false
-
-# setup test files
 reporter = Minitest::Ci.new $ci_io, { :report_name => :sha1 }
 reporter.start
 Minitest.__run reporter, {}
 reporter.report
 
-class TestMinitest; end
-class TestMinitest::TestOptions < Minitest::Test
+class TestOptions < Minitest::Test
   def test_filename_is_sha1_digest
-    sha1 = Digest::SHA1.hexdigest("report with this name should be transformed in sha1")
+    sha1 = Digest::SHA1.hexdigest(TestOptionsDescription)
     assert File.exist? "test/reports/TEST-#{sha1}.xml"
   end
 end
 
-class MockTestProcReportNameSuite < Minitest::Test
+describe 'report with this name should be reversed' do
+  it 'passes' do
+    pass
+  end
+end
+
+class TestPassAgain < Minitest::Test
   def test_pass
     pass
   end
 end
 
-describe 'report with this name should be reversed' do
- it 'passes' do
-   pass
- end
-end
-
+# Resetup test reporter using Proc
 $ci_io = StringIO.new
 Minitest::Ci.clean = false
-
-# setup test files
-reporter = Minitest::Ci.new $ci_io, { :report_name => -> (name) { "TEST-#{CGI.escape(name.to_s.gsub(/\W+/, '_').reverse)[0, 246]}.xml" } }
+reporter = Minitest::Ci.new $ci_io, {
+  :report_name => Proc.new do |name|
+    "TEST-#{CGI.escape(name.to_s.gsub(/\W+/, '_').reverse)[0, 246]}.xml"
+  end
+}
 reporter.start
 Minitest.__run reporter, {}
 reporter.report
 
-# Minitest::Runnable.reset
-
-class TestMinitest; end
-class TestMinitest::TestOptions < Minitest::Test
+class TestOptionsProc < Minitest::Test
   def test_filename_is_from_given_proc
     assert File.exist? "test/reports/TEST-desrever_eb_dluohs_eman_siht_htiw_troper.xml"
   end
